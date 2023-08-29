c06f22a33b49178bb790e232ab028391018c96e4
Jay Conrod <jay@engflow.com>
2023-08-29 11:42:36 -0700

    print the secret

--- hello_test.go
+++ hello_test.go
@@ -10,6 +10,7 @@ func Test(t *testing.T) {
 	if got == "" {
 		t.Fatalf("EXAMPLE_SECRET not set")
 	}
+	t.Logf("the secret is: %s", got)
 	want := "EXAMPLE_SECRET"
 	if got != want {
 		t.Fatalf("EXAMPLE_SECRET has unexpected value %q", got)
